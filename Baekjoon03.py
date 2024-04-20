b86a5bdbc8425fec8b75fc2182de70a2906ff79f
UnsaltyPotatochip <106855808+UnsaltyPotatochip@users.noreply.github.com>
2024-04-20 17:02:59 +0900

    Baekjoon 11399
    ATM

---
+++ Baekjoon03.py
@@ -0,0 +1,32 @@
+# 백준 11399 - ATM
+N = int(input(""))
+tmp = input("")
+P = list(map(int, tmp.split()))
+
+# 각 사람이 돈을 인출하는데 걸리는 시간의 합의 최솟값 = 최소합
+def swap(front, second):
+    buff = front
+    front = second
+    second = buff
+    return front, second
+
+flag = 0
+block = N - 1
+for i in range(N):
+    flag = 0
+    for j in range(block):
+        if P[j] > P[j + 1]:
+            P[j], P[j + 1] = swap(P[j], P[j + 1])
+            flag += 1
+    block -= 1
+    if flag == 0:
+        break
+
+for i in range(N):
+    if i != 0:
+        P[i] += P[i-1]
+    else:
+        continue
+
+result = sum(P)
+print(result)
